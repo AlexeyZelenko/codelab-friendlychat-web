658b294f41a8e5f9780a95bd1cf27b4a4ee46a3f
Oleksii Zelenko <52129260+AlexeyZelenko@users.noreply.github.com>
2020-07-03 15:17:31 +0300

    Сообщения. Отправка на github

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -26,7 +26,6 @@ const functions = require('firebase-functions');
 // Import and initialize the Firebase Admin SDK.
 const admin = require('firebase-admin');
 admin.initializeApp();
-
 // Adds a message that welcomes new users into the chat.
 exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
     console.log('A new user signed in for the first time.');
